a3158350bf3d0c0087f779f6c1a30175a412e433
Wixxo Tech <92201052+wixxotech@users.noreply.github.com>
2024-09-11 09:17:34 +0530

    Update sms6.py

--- sms6.py
+++ sms6.py
@@ -313,7 +313,7 @@ def smsgf(number):
         }
     ]
 
-    for _ in range(30):  # Reducing the number of iterations
+    for _ in range(5):  # Reducing the number of iterations
         for api in apis:
             try:
                 if api["method"] == "POST":
