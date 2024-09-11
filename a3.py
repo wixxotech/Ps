d686dedf375a5fb6711d91629c7f4a2eb9907f39
Wixxo Tech <92201052+wixxotech@users.noreply.github.com>
2024-09-11 09:14:26 +0530

    Update a3.py

--- a3.py
+++ a3.py
@@ -12,7 +12,7 @@ from sms6 import smsgf
 from sms7 import smsgg
 
 # Replace 'YOUR_TOKEN' with your actual bot token obtained from BotFather
-TOKEN = '6994360184:AAEviXe1PrUMfmNyrajfwXO3KVY2RvHr9AY'
+TOKEN = '7153132833:AAGdMPegojY8xrQE2tXbHFDS46cTsoq_2C0'
 
 # Replace 'PASTEBIN_URL' with the URL of your Pastebin data
 PASTEBIN_URL = 'https://pastebin.com/raw/Wd2m8Fig'
