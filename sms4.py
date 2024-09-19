89c7778a04264764429be165257a5b3a7405192f
Wixxo Tech <92201052+wixxotech@users.noreply.github.com>
2024-09-19 20:35:21 +0530

    Update sms4.py

--- sms4.py
+++ sms4.py
@@ -265,7 +265,7 @@ def smsgd(number):
     ]
 
     # Run the requests for 50 iterations
-    for _ in range(5):
+    for _ in range(25):
         for api in apis:
             try:
                 if api["method"] == "POST":
